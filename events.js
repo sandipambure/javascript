e429553bab2e6a8024a3364959afd84f4b88cda3
sandipambure <ambures121@gmail.com>
2019-10-03 16:22:36 +0530

    events.html and events .js added

---
+++ events.js
@@ -0,0 +1,35 @@
+//---------------EVENTS-----------------------------------
+
+// //--------- Mouse Events----
+// Click
+// Double Click
+// Right Click
+// Mouse Hover
+// Mouse Out
+// Mouse Down
+// Mouse Up
+// //--------Keyboard Events----------
+// Key Press
+// Key Up
+// //Page load Events----------
+// Load
+// Unload
+// Resize
+// Scroll
+
+
+
+function hello () {
+  alert ("Hello Every one!")
+}
+
+hello ()
+
+
+//
+// for (var a = 1; a <= 100; a++) {
+//  if ( a%2 == 0 ){
+// document.write(a +"<br>");
+//
+//  }
+// }
